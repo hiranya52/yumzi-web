63c55e899a5a6a562d269ba7a1d1ad475f451b43
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-03-27 19:55:35 +0530

    Implement website footer component

--- yumzi/src/app/Pages/home/home.ts
+++ yumzi/src/app/Pages/home/home.ts
@@ -3,11 +3,12 @@ import { HeroComponent } from '../../Components/hero-component/hero-component';
 import { FlavorBombsSection } from "../../Components/flavor-bombs-section/flavor-bombs-section";
 import { BrandStorySection } from "../../Components/brand-story-section/brand-story-section";
 import { NewsletterCTASection } from "../../Components/newsletter-ctasection/newsletter-ctasection";
+import { Footer } from "../../Components/footer/footer";
 
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, FlavorBombsSection, BrandStorySection, NewsletterCTASection],
+  imports: [HeroComponent, FlavorBombsSection, BrandStorySection, NewsletterCTASection, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
